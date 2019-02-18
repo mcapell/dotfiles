341159a006d045810666d658943e708c242b94dc
Marc Capell <marc.capell@gmail.com>
2019-02-18 13:53:14 +0100

    Some changes after upgrading to ubuntu 18.04

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ ln -s $HOMEDIR/.dotfiles/git/gitignore_global $HOMEDIR/.gitignore_global
 # shell/
 ln -s $HOMEDIR/.dotfiles/shell/bash_aliases $HOMEDIR/.bash_aliases
 ln -s $HOMEDIR/.dotfiles/shell/profile $HOMEDIR/.profile
+ln -s $HOMEDIR/.dotfiles/shell/inputrc $HOMEDIR/.inputrc
 
 # tmux/
 ln -s $HOMEDIR/.dotfiles/tmux/tmux.conf $HOMEDIR/.tmux.conf
